5f733599ad4526b5fde2e0f491d1c3cc7684431c
Indra Nallappan <back2indra@gmail.com>
2019-04-23 19:29:01 -0500

    Updates

---
+++ precache-manifest.62edfbd35f113bd75a2e3cdef7012e08.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccb55e695d3f8aa32b9a31e0ce133e25",
+    "url": "/clicky/index.html"
+  },
+  {
+    "revision": "755ba488f4918dee673d",
+    "url": "/clicky/static/css/main.7eb83813.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clicky/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "755ba488f4918dee673d",
+    "url": "/clicky/static/js/main.34824ec5.chunk.js"
+  },
+  {
+    "revision": "e4363465db862e89c37e",
+    "url": "/clicky/static/js/runtime~main.c6956f21.js"
+  }
+]);
